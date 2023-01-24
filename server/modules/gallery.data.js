c3c20da719d6b1692bc613715cb24b86fbd8cfbe
Linssi Colin Ruiz <linssi101@gmail.com>
2023-01-24 17:16:29 -0600

    added extra data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,19 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+	{
+		id: 1,
+		path: "images/goat_small.jpg",
+		description: "Photo of a goat taken at Glacier National Park.",
+		likes: 0,
+	},
+	{
+		id: 2,
+		path: "images/goat_small.jpg",
+		description: "Photo of a goat.",
+		likes: 0,
+	},
 ];
 
 module.exports = galleryItems;
+
+// convert into pool when base is done.
+// pg
