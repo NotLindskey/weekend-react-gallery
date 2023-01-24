a9e925363c1724762f7b23885bc0758d572ffc94
Linssi Colin Ruiz <linssi101@gmail.com>
2023-01-24 17:17:05 -0600

    checks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
+import Header from '../Header/Header';
+import GalleryList from '../GalleryList/GalleryList';
+
 import './App.css';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    // App loaded
+    console.log('App is loaded');
+
+    // display list on DOM
+    displayGalleryList();
+  },[])   
+
+  // GET data from /gallery
+  const displayGalleryList = () => {
+    axios.get('/gallery')
+    .then(( response ) => {
+      // console.log(response.data);
+      setGalleryList(response.data);
+    })
+    .catch((error) => {
+      console.log('error retrieving ',error);
+    })
+  
+  }
+
+  // const galleryList = (galleryList) => {
+  //   console.log(galleryList);
+  // }
+
+  // DOM
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <Header />
+        <GalleryList 
+        galleryList={galleryList} />
+        
       </div>
     );
 }
